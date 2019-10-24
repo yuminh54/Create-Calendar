9efdd47243683e7f74d1333b2f8957edcde4bc37
yejikk <dpwl7484@gmail.com>
2019-10-24 16:52:27 +0900

    setting 변경

--- mysql.js
+++ mysql.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   port     : 5000,
-  password : 'password',
+  password : '12345',
   database : 'my_database'
 });
  
